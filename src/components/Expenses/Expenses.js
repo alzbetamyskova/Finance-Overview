9a70cdb7dc1b72eb274443b0f3692d737eca4dd2
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-08-13 13:34:55 +0200

    přidán formulář pro přidávání útraty

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import ExpenseItem from "./ExpenseItem";
+
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
 
   return (
     <div className='expenses'>
